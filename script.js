2dd0634a2151c962f1f24eddca8186478d7a8bd2
LATEEF AHMAD BABA <91480408+Mohdlateef@users.noreply.github.com>
2023-11-14 19:12:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,21 +2,21 @@ function isSameType(value1, value2) {
   //your js code here
 	if(typeof(value1)==number&&typeof(value2)==number)
 	{
-		return true;
+		return "true";
 	}
 	if(typeof(value1)==string&&typeof(value2)==string)
 	{
-		return false;
+		return "true";
 	}
 	else
 	{
-		return true;
+		return "false";
 	}
 	
 	
 }
 
 // do not change the code below.
-let value1 = prompt("Enter Start of the Range.");
-let value2 = prompt("Enter End Of the Range.");
-alert(isSameType(value1, value2));
+// let value1 = prompt("Enter Start of the Range.");
+// let value2 = prompt("Enter End Of the Range.");
+// alert(isSameType(value1, value2));
